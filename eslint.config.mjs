fa5109730e4f44c3c6645934ce8ee1ff4f46ac23
javalce <javalce29@gmail.com>
2024-09-18 10:58:37 +0200

    refactor: use new eslint config file

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import eslint from '@eslint/js';
+import astro from 'eslint-plugin-astro';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config} */
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...astro.configs['recommended'],
+  ...astro.configs['jsx-a11y-recommended'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.es2022,
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaVersion: 2022,
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      'padding-line-between-statements': [
+        'warn',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+      ],
+    },
+  },
+);

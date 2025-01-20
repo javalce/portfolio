02c6a326790faa0b70ebee4c11556d598cfb2c60
Javier Valero Cejudo <javalce29@gmail.com>
2025-01-20 14:56:38 +0100

    build: update Prettier configuration and dependencies

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from '@javalce/prettier-config';
+
+export default defineConfig({
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+});

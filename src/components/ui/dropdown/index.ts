5a4949cdf75767cce252d99cf7b64f5153366c79
Javier Valero Cejudo <javalce29@gmail.com>
2025-05-08 22:28:57 +0200

    feat: implement dropdown component with content, item, label, separator, and trigger

---
+++ src/components/ui/dropdown/index.ts
@@ -0,0 +1,15 @@
+import DropdownContent from './dropdown-content.astro';
+import DropdownItem from './dropdown-item.astro';
+import DropdownLabel from './dropdown-label.astro';
+import DropdownSeparator from './dropdown-separator.astro';
+import DropdownTrigger from './dropdown-trigger.astro';
+import Dropdown from './dropdown.astro';
+
+export {
+  Dropdown,
+  DropdownContent,
+  DropdownItem,
+  DropdownLabel,
+  DropdownSeparator,
+  DropdownTrigger,
+};

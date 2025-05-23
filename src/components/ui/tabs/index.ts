c6fcf85f858114f63e814f15400f65abd7ae64a6
Javier Valero Cejudo <javalce29@gmail.com>
2025-05-23 13:59:13 +0200

    feat: add tabs component with content, list, and trigger functionality

---
+++ src/components/ui/tabs/index.ts
@@ -0,0 +1,6 @@
+import TabsContent from './tabs-content.astro';
+import TabsList from './tabs-list.astro';
+import TabsTrigger from './tabs-trigger.astro';
+import Tabs from './tabs.astro';
+
+export { Tabs, TabsContent, TabsList, TabsTrigger };

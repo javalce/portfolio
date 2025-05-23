d72a27cf82eb48f3516b2c19ffbf0a121c777fe6
Javier Valero Cejudo <javalce29@gmail.com>
2025-05-23 13:37:17 +0200

    chore: add tailwindStylesheet configuration to prettier

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from '@javalce/prettier-config';
 
 export default defineConfig({
   plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/styles/globals.css',
   overrides: [
     {
       files: '*.astro',

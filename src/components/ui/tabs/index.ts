478b01ed43eebd970c21b1a9d055648ad2d5f4a2
Javier Valero Cejudo <javalce29@gmail.com>
2025-12-08 15:27:47 +0100

    chore: remove unused components

--- src/components/ui/tabs/index.ts
+++
@@ -1,6 +0,0 @@
-import TabsContent from './tabs-content.astro';
-import TabsList from './tabs-list.astro';
-import TabsTrigger from './tabs-trigger.astro';
-import Tabs from './tabs.astro';
-
-export { Tabs, TabsContent, TabsList, TabsTrigger };

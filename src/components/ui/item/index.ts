96085f96194f7da3981bafd584842589ea52eccf
Javier Valero Cejudo <javalce29@gmail.com>
2025-12-08 13:22:28 +0100

    feat: add item component

---
+++ src/components/ui/item/index.ts
@@ -0,0 +1,23 @@
+import ItemActions from './item-actions.astro';
+import ItemContent from './item-content.astro';
+import ItemDescription from './item-description.astro';
+import ItemFooter from './item-footer.astro';
+import ItemGroup from './item-group.astro';
+import ItemHeader from './item-header.astro';
+import ItemMedia from './item-media.astro';
+import ItemSeparator from './item-separator.astro';
+import ItemTitle from './item-title.astro';
+import Item from './item.astro';
+
+export {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemFooter,
+  ItemGroup,
+  ItemHeader,
+  ItemMedia,
+  ItemSeparator,
+  ItemTitle,
+};

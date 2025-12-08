427a191d1b789798f17168525b723dbc5e92d049
Javier Valero Cejudo <javalce29@gmail.com>
2025-12-08 15:24:06 +0100

    build: add astro-icon dependency

--- astro.config.js
+++ astro.config.js
@@ -1,4 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -6,4 +7,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [icon()],
 });

88251d2403a8139d392e7bd11a236b50c5026c5c
Javier Valero Cejudo <javalce29@gmail.com>
2026-02-05 20:59:10 +0100

    feat: add breadcrumb component

---
+++ src/components/ui/breadcrumb/index.ts
@@ -0,0 +1,17 @@
+import BreadcrumbEllipsis from './breadcrumb-ellipsis.astro';
+import BreadcrumbItem from './breadcrumb-item.astro';
+import BreadcrumbLink from './breadcrumb-link.astro';
+import BreadcrumbList from './breadcrumb-list.astro';
+import BreadcrumbPage from './breadcrumb-page.astro';
+import BreadcrumbSeparator from './breadcrumb-separator.astro';
+import Breadcrumb from './breadcrumb.astro';
+
+export {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+};

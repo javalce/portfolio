bb6413a6fb17caa0525e35269ef6bd4b424c8f2b
Javier Valero Cejudo <javalce29@gmail.com>
2026-04-06 21:31:57 +0200

    feat: add bento card component

---
+++ src/components/ui/bento-card/index.ts
@@ -0,0 +1,15 @@
+import BentoCardContent from './bento-card-content.astro';
+import BentoCardDescription from './bento-card-description.astro';
+import BentoCardFooter from './bento-card-footer.astro';
+import BentoCardImage from './bento-card-image.astro';
+import BentoCardTitle from './bento-card-title.astro';
+import BentoCard from './bento-card.astro';
+
+export {
+  BentoCard,
+  BentoCardContent,
+  BentoCardDescription,
+  BentoCardFooter,
+  BentoCardImage,
+  BentoCardTitle,
+};
